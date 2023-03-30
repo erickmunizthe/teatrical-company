365856a01cf7e20aba05db3ee1f794b9d145dadd
erick.muniz <erick.muniz@softplan.com.br>
2023-03-30 17:10:25 -0300

    Versão original

---
+++ App.js
@@ -0,0 +1,7 @@
+const statement = require("./src/statement.js");
+const invoices = require('./data/invoices.json');
+const plays = require('./data/plays.json');
+
+for (let invoice of invoices) {
+    console.log(statement(invoice,plays));
+}
