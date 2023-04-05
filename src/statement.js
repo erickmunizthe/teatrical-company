ef770844187a561c49dc5c9fdce1f1ff9a13fc7c
erick.muniz <erick.muniz@softplan.com.br>
2023-04-05 00:20:25 -0300

    Remoção da variavel volumeCredits

--- src/statement.js
+++ src/statement.js
@@ -2,18 +2,17 @@ const plays = require("../data/plays.json");
 
 function statement(invoice) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
     // print line for this order
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
+
   result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
   return result;
 }
 
@@ -56,7 +55,15 @@ function usd(aNumber) {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
-  }).format(aNumber/100);
+  }).format(aNumber / 100);
+}
+
+function totalVolumeCredits(invoice) {
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+  return volumeCredits;
 }
 
 module.exports = statement;
