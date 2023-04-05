27115aa97a264f9824d61a6ff0457dd30c4869ad
erick.muniz <erick.muniz@softplan.com.br>
2023-04-05 01:41:58 -0300

    Extração da logica de criação dos dados

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-const statement = require("./src/statement.js");
+const renderPlainText = require("./src/statement.js");
 const invoices = require('./data/invoices.json');
 const plays = require('./data/plays.json');
 
 for (let invoice of invoices) {
-    console.log(statement(invoice));
+    console.log(renderPlainText(invoice));
 }
